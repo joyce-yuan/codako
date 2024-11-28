1c86fbdfc62ad7731da8dab68494d6cdc38c2b9f
Ben Gotow <ben@foundry376.com>
2024-11-28 16:04:22 -0600

    Make cookies last way longer before expiring (1 year)

--- frontend/src/helpers/session-storage.js
+++ frontend/src/helpers/session-storage.js
@@ -2,6 +2,8 @@
 import Cookies from 'js-cookie';
 
 const KEY = 'session';
+const ONE_DAY = 24 * 60 * 60 * 1000;
+
 let value = Cookies.get(KEY);
 
 export const sessionStorageMiddleware = store => next => action => {
@@ -10,7 +12,9 @@ export const sessionStorageMiddleware = store => next => action => {
   const {me} = store.getState();
   const nextValue = JSON.stringify({me});
   if (value !== nextValue) {
-    Cookies.set(KEY, nextValue);
+    Cookies.set(KEY, nextValue, {
+      expires: new Date(Date.now() + ONE_DAY * 365),
+    });
     value = nextValue;
   }
 
