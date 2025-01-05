f8d2bd8ef741b60bc81d2621a40ce43e2163d906
Ben Gotow <ben@foundry376.com>
2025-01-04 22:23:37 -0600

    Support standard rotations as well as flipping actors using “Direction” variable

--- frontend/src/editor/components/sprites/sprite.js
+++ frontend/src/editor/components/sprites/sprite.js
@@ -1,5 +1,4 @@
-import React, {PropTypes} from 'react';
-
+import React, { PropTypes } from "react";
 
 export default class Sprite extends React.Component {
   static propTypes = {
@@ -9,31 +8,35 @@ export default class Sprite extends React.Component {
     spritesheet: PropTypes.object.isRequired,
     appearance: PropTypes.string,
     frame: PropTypes.number,
-  }
+  };
 
   render() {
-    const {appearance, transform, frame, spritesheet, className} = this.props;
-    const {width, appearances} = spritesheet;
+    const { appearance, transform, frame, spritesheet, className } = this.props;
+    const { width, appearances } = spritesheet;
 
-    let data = require('../../img/splat.png');
+    let data = require("../../img/splat.png");
     if (appearance && appearances[appearance]) {
       data = appearances[appearance][frame || 0];
     }
 
-    const style = Object.assign({
-      width: width,
-      height: width,
-      display: 'inline-block',
-      transform: {
-        'flip-x': 'scale(-1, 1)',
-        'flip-y': 'scale(1, -1)',
-        'flip-xy': 'scale(-1, -1)',
-        'none': undefined,
-      }[transform],
-    }, this.props.style);
-
-    return (
-      <img src={data} className={`sprite ${className}`} style={style} />
+    const style = Object.assign(
+      {
+        width: width,
+        height: width,
+        display: "inline-block",
+        transform: {
+          "90deg": "rotate(90deg)",
+          "180deg": "rotate(180deg)",
+          "270deg": "rotate(270deg)",
+          "flip-x": "scale(-1, 1)",
+          "flip-y": "scale(1, -1)",
+          "flip-xy": "scale(-1, -1)",
+          none: undefined,
+        }[transform],
+      },
+      this.props.style
     );
+
+    return <img src={data} className={`sprite ${className}`} style={style} />;
   }
 }
