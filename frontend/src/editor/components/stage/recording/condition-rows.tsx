1bd21f113b09b0e827690c803920017f9eb8d7b5
Ben Gotow <ben@foundry376.com>
2025-07-12 21:24:39 -0400

    Fix Sentry error: missing react key

--- frontend/src/editor/components/stage/recording/condition-rows.tsx
+++ frontend/src/editor/components/stage/recording/condition-rows.tsx
@@ -283,7 +283,9 @@ const ComparatorSelect = ({
     {Object.entries(ComparatorLabels)
       .filter((t) => (impliedDatatype ? ["=", "!="].includes(t[0]) : true))
       .map(([key, value]) => (
-        <option value={key}>{value}</option>
+        <option key={key} value={key}>
+          {value}
+        </option>
       ))}
   </select>
 );
