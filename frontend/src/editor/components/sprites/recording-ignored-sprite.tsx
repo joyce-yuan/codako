b2b2e2afba412e65d7b135740406c1f2da6cdbb7
Ben Gotow <ben@foundry376.com>
2025-07-18 11:12:50 -0500

    Fix rendering of the “ignored squares” in production, image URL wrong

--- frontend/src/editor/components/sprites/recording-ignored-sprite.tsx
+++ frontend/src/editor/components/sprites/recording-ignored-sprite.tsx
@@ -6,7 +6,7 @@ const RecordingIgnoredSprite = ({ x, y }: { x: number; y: number }) => {
       style={{
         position: "absolute",
         pointerEvents: "none",
-        background: `url('/src/editor/img/ignored_square.png') top left no-repeat`,
+        background: `url('${new URL("../../img/ignored_square.png", import.meta.url).href}') top left no-repeat`,
         width: STAGE_CELL_SIZE,
         height: STAGE_CELL_SIZE,
         left: x * STAGE_CELL_SIZE,
